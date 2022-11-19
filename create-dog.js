632d3d94cef6fef2d699211fe4e81e9bafb0e323
Miggorie <info@elinselin.com>
2022-11-19 02:41:51 +0100

    Redo page in js instead and learning about XML-req

---
+++ create-dog.js
@@ -0,0 +1,38 @@
+const inputDogName = document.querySelector("#dogName");
+const inputReg = document.querySelector("#regNummer");
+const inputUrl = document.querySelector("#hundUrl");
+const inputTail = document.querySelector("#dogTail");
+const inputBirth = document.querySelector("#dogBirth");
+const inputColor = document.querySelector("#dogColor");
+const inputCountry = document.querySelector("#dogUrl");
+const submitDogBtn = document.querySelector("#submitDogBtn");
+
+// submitDogBtn.addEventListener("click", () => {
+//   let xhr = new XMLHttpRequest();
+//   console.log("UNSENT", xhr.readyState);
+
+//   xhr.open("GET", "./message.txt", true);
+// });
+
+loadEventListeners();
+
+function loadEventListeners() {
+  submitDogBtn.addEventListener("click", submitDog);
+
+  // inputTail.addEventListener("click", dogTail);
+}
+
+function submitDog() {
+  let xhr = new XMLHttpRequest();
+  console.log("UNSENT", xhr.readyState);
+
+  xhr.open("GET", "./message.txt", true);
+
+  console.log("OPENED", xhr.readyState);
+
+  xhr.onload = () => {
+    console.log("DONE", xhr.readyState);
+  };
+
+  xhr.send();
+}
